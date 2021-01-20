b7117e97edb382f12d0c77fea89e8223b2aef0f1
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2021-01-19 21:18:03 -0600

    finished module 7 wesbos

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,18 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallBack(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallBack, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
